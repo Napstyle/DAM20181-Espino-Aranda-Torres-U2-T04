aa0bc59772b78ae2e78103e37bbf1d24dbca085e
Cesar Ramses Espino Serrano <ceraespinose@ittepic.edu.mx>
2018-03-19 18:53:48 -0700

    Agragando Listas

--- ToDo/src/pages/home/home.ts
+++ ToDo/src/pages/home/home.ts
@@ -6,6 +6,11 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  notes=[
+    {id:1,title:'nota1',description:'Descricion de la nota1'},
+    {id:2,title:'nota2',description:'Descricion de la nota2'},
+    {id:3,title:'nota3',description:'Descricion de la nota3'}
+  ];
 
   constructor(public navCtrl: NavController) {
 
