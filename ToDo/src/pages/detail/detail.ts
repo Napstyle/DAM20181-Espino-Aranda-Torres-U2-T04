77eef228eb84cde9b16e7ec128b0708f69d12b7f
Cesar Ramses Espino Serrano <ceraespinose@ittepic.edu.mx>
2018-03-20 18:50:39 -0700

    Agragando el boton 'save' e inputs a la pagina de detalle

--- ToDo/src/pages/detail/detail.ts
+++ ToDo/src/pages/detail/detail.ts
@@ -15,6 +15,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class DetailPage {
 
+  task={id:null,title:null,description:null};
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
